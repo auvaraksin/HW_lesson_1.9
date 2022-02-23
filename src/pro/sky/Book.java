0252a7c9e181d2c15646c8417996c87962dabb48
auvararaksin <auvaraksin@mail.ru>
2022-02-23 23:20:40 +0200

    HW_lesson_1.10 initial version

--- src/pro/sky/Book.java
+++ src/pro/sky/Book.java
@@ -1,5 +1,7 @@
 package pro.sky;
 
+import java.util.Objects;
+
 public class Book {
     private String bookName;
     private Author authorName;
@@ -11,19 +13,37 @@ public class Book {
         this.yearPublishing = yearPublishing;
     }
 
-     public String getBookName() {
-        return this.bookName;
+    public String getBookName() {
+        return bookName;
     }
 
     public Author getAuthorName() {
-        return this.authorName;
+        return authorName;
     }
 
     public int getYearPublishing() {
-        return this.yearPublishing;
+        return yearPublishing;
     }
 
     public void setYearPublishing(int yearPublishing) {
         this.yearPublishing = yearPublishing;
+    }
+
+    @Override
+    public String toString() {
+        return bookName + " - " + authorName + ", " + yearPublishing;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearPublishing == book.yearPublishing && bookName.equals(book.bookName) && authorName.equals(book.authorName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, authorName, yearPublishing);
     }
 }
